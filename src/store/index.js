8941cf4dcf314567b5cfff88012c009b865aeec4
karimishiva <karimishiva07@gmail.com>
2023-08-22 16:45:17 +0330

    [ADD] handle reset button actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
-import {configureStore,createSlice} from '@reduxjs/toolkit'
+import {configureStore,createSlice,createAction} from '@reduxjs/toolkit'
 
+export const reset=createAction('app/reset')
 const moviesSlice=createSlice({
     name:'movie',
     initialState:[],
@@ -11,6 +12,11 @@ const moviesSlice=createSlice({
             const index=state.indexOf(action.payload)
             state.splice(index,1)
         }
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(reset,(state,action)=>{
+            return []
+        })
     }
 })
 
@@ -25,6 +31,11 @@ const songsSlice=createSlice({
            const index=state.indexOf(action.payload)
            state.splice(index,1)
         }
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(reset,(state,action)=>{
+            return []
+        })
     }
 })
 
